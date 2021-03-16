7c41d2feb6329e88742b55c515f5261a69db8a09
Katelynn Cowan <kcowan@twu.edu>
2021-03-15 20:10:55 -0500

    Added connection code to connect mySQL database and arrays for lists of employees, roles and managers

--- index.js
+++ index.js
@@ -2,3 +2,23 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 const consoleTable = require("console.table");
+
+// connection to mySQL
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "employee_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    startPrompt();
+});
+
+// variables
+var roleList = [];
+var managerList = [];
+var employeeList = [];
