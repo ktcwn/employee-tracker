e8acf57d13c927ac8dfd52d1072c5da72c8c7f75
Katelynn Cowan <kcowan@twu.edu>
2021-03-15 20:19:47 -0500

    created select functions and called back at the end of the script. Added department and update functions

--- index.js
+++ index.js
@@ -150,3 +150,146 @@ const addEmployee = () => {
                 })
         })
 };
+
+// add role
+const addRole = () => {
+    inquirer.prompt([
+        {
+            name: "title",
+            type: "input",
+            message: "What is the title of the role?",
+            validate: validInput
+        },
+        {
+            name: "salary",
+            type: "input",
+            message: "What is the salary of the role?",
+            validate: validInput
+        },
+        {
+            name: "deptID",
+            type: "input",
+            message: "What is the department ID?",
+            validate: validInput
+        },
+    ])
+        .then(function (answer) {
+            connection.query("INSERT INTO roles SET ?",
+                {
+                    title: answer.title,
+                    salary: answer.salary,
+                    department_id: answer.deptID
+                },
+                function (err) {
+                    if (err) throw err
+                    console.table(answer);
+                    startPrompt();
+                })
+        })
+}
+
+// add department
+const addDepartment = () => {
+    inquirer.prompt([
+        {
+            name: "deptName",
+            type: "input",
+            message: "What is the department name",
+            validate: validInput
+        }
+    ])
+        .then(function (answer) {
+            connection.query("INSERT INTO department SET ?",
+                {
+                    name_dept: answer.deptName
+                },
+                function (err) {
+                    if (err) throw err
+                    console.table(answer);
+                    startPrompt();
+                })
+        })
+}
+
+// update employee roles
+const updateEmployeeRoles = () => {
+    connection.query("SELECT * FROM employee;", function (err, res) {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                name: "lastName",
+                type: "rawlist",
+                choices: selectEmployee(),
+                message: "Which employee's role are you changing?"
+            },
+            {
+                name: "roles",
+                type: "rawlist",
+                choices: selectRole(),
+                message: "What is the employee's new role?"
+            }
+        ])
+            .then(function (answer) {
+                const roleID = selectRole().indexOf(answer.roles) + 1;
+                console.log(roleID);
+                connection.query("UPDATE employee SET role_id = ? WHERE last_name = ?",
+                    [
+                        roleID,
+                        answer.lastName
+                    ],
+                    function (err) {
+                        if (err) throw err;
+                        console.table(answer);
+                        startPrompt();
+                    })
+            });
+    });
+}
+
+// select functions
+const selectRole = () => {
+    connection.query("SELECT * FROM roles", function (err, res) {
+        if (err) throw err;
+        roleList = [];
+        for (var i = 0; i < res.length; i++) {
+            roleList.push(res[i].title);
+        }
+    })
+    return roleList;
+};
+
+const selectManager = () => {
+    connection.query("SELECT * FROM employee WHERE manager_id IS NULL", function (err, res) {
+        if (err) throw err;
+        managerList = [];
+        for (var i = 0; i < res.length; i++) {
+            managerList.push(res[i].first_name);
+        }
+    })
+    return managerList;
+};
+
+const selectEmployee = () => {
+    connection.query("SELECT * FROM employee", function (err, res) {
+        if (err) throw err;
+        employeeList = [];
+        for (var i = 0; i < res.length; i++) {
+            employeeList.push(res[i].last_name);
+        }
+    })
+    return employeeList;
+};
+
+
+const validInput = (input, type) => {
+    if (input.length < 1) {
+        return console.log(`Please provide a valid input.`);
+    }
+    return true;
+}
+
+
+// call functions
+selectRole();
+selectManager();
+selectEmployee();
