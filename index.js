da01dfe3367281a67ec0996805650284129c0688
Katelynn Cowan <kcowan@twu.edu>
2021-03-15 20:14:03 -0500

    Started inquirer code to generate a list of questions to ask the user within employee directory

--- index.js
+++ index.js
@@ -22,3 +22,57 @@ connection.connect(function (err) {
 var roleList = [];
 var managerList = [];
 var employeeList = [];
+
+// begin inquirer
+const startPrompt = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Employee Directory",
+            name: "choice",
+            choices: [
+                "View employees by name",
+                "View employees by role",
+                "View employees by department",
+
+                "Add employee",
+                "Add role",
+                "Add department",
+                "Update employee role",
+
+                "Finished"
+            ]
+        }
+    ])
+        .then((answer) => {
+            switch (answer.choice) {
+                case "View employees by name":
+                    viewEmployees();
+                    break;
+                case "View employees by role":
+                    viewRoles();
+                    break;
+                case "View employees by department":
+                    viewDepartments();
+                    break;
+
+                case "Add employee":
+                    addEmployee();
+                    break;
+                case "Add role":
+                    addRole();
+                    break;
+                case "Add department":
+                    addDepartment();
+                    break;
+                case "Update employee role":
+                    updateEmployeeRoles();
+                    break;
+                case "Finished":
+                    process.exit();
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
